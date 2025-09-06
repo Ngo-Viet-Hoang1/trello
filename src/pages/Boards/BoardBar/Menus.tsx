54e9a4905e5147bc91e33cedc7d74a33c3d29c6d
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-09-06 21:51:00 +0700

    feat: enhance BoardBar with new components and layout adjustments

---
+++ src/pages/Boards/BoardBar/Menus.tsx
@@ -0,0 +1,146 @@
+import BackupTableIcon from '@mui/icons-material/BackupTable'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import ClearIcon from '@mui/icons-material/Clear'
+import LockOutlineIcon from '@mui/icons-material/LockOutline'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import TimelineIcon from '@mui/icons-material/Timeline'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import ClickAwayListener from '@mui/material/ClickAwayListener'
+import Fade from '@mui/material/Fade'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import Popper from '@mui/material/Popper'
+import Tooltip from '@mui/material/Tooltip'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import ListItemIcon from '~/assets/ListItemIcon'
+
+export default function Menus() {
+  const [open, setOpen] = React.useState(false)
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+    setOpen((previousOpen) => !previousOpen)
+  }
+
+  const canBeOpen = open && Boolean(anchorEl)
+  const id = canBeOpen ? 'transition-popper' : undefined
+
+  return (
+    <div>
+      <Tooltip title="Views">
+        <ClickAwayListener onClickAway={() => setOpen(false)}>
+          <IconButton onClick={handleClick} sx={{ borderRadius: 1 }}>
+            <MoreHorizIcon fontSize="small" />
+          </IconButton>
+        </ClickAwayListener>
+      </Tooltip>
+
+      <Popper id={id} open={open} anchorEl={anchorEl} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Box
+              sx={{
+                position: 'relative',
+                bgcolor: 'background.paper',
+                border: '1px solid #A6C5E229',
+                boxShadow: 3,
+                p: 2,
+                borderRadius: 1,
+                width: 300,
+              }}
+            >
+              <Box>
+                <Typography
+                  sx={{
+                    textAlign: 'center',
+                    fontWeight: 500,
+                    color: 'primary.main',
+                  }}
+                >
+                  Upgrade for Views
+                </Typography>
+                <IconButton
+                  size="small"
+                  onClick={() => setOpen(false)}
+                  sx={{
+                    position: 'absolute',
+                    top: 10,
+                    right: 10,
+                    borderRadius: 1,
+                  }}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </Box>
+
+              <Box
+                sx={{ gap: 2, display: 'flex', flexDirection: 'column', mt: 2 }}
+              >
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <ListItemIcon />
+                  Board
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <BackupTableIcon />
+                    Table
+                  </Box>
+                  <LockOutlineIcon fontSize="small" />
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <CalendarMonthIcon />
+                    Calendar
+                  </Box>
+                  <LockOutlineIcon fontSize="small" />
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <TimelineIcon />
+                    Timeline
+                  </Box>
+                  <LockOutlineIcon fontSize="small" />
+                </Box>
+              </Box>
+
+              <Box
+                sx={{ display: 'flex', flexDirection: 'column', gap: 4, mt: 2 }}
+              >
+                <Typography>See your work in new ways</Typography>
+                <Typography>
+                  View key timelines, assignments, data, and more directly from
+                  your Trello board with Trello Premium.
+                </Typography>
+                <Button variant="contained">Start free trial</Button>
+                <Link sx={{ textAlign: 'center' }} href="#">
+                  Learn more about Trello Premium
+                </Link>
+              </Box>
+            </Box>
+          </Fade>
+        )}
+      </Popper>
+    </div>
+  )
+}
--- src/pages/Boards/BoardBar/index.tsx
+++ src/pages/Boards/BoardBar/index.tsx
@@ -1,17 +1,58 @@
 import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Views from './Views'
+import Avatars from './Avatars'
+import Menus from './Menus'
+import IconButton from '@mui/material/IconButton'
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt'
 
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.dark',
         width: '100%',
         height: (theme) => theme.trello.boardBarHeight,
         display: 'flex',
         alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingX: 2,
+        boxSizing: 'border-box',
+        overflowX: 'auto',
+        borderTop: '1px solid #A6C5E229',
       }}
     >
-      Board Bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small"
+          size="small"
+          sx={{
+            '& .MuiInputBase-input': { textAlign: 'center' },
+            '& fieldset': { border: 'none' },
+            '&:hover fieldset': { bgcolor: 'rgba(0, 0, 0, 0.1)' },
+          }}
+          value={'Untitled'}
+        />
+        <Views />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Avatars />
+        <IconButton
+          sx={{
+            fontSize: 14,
+            borderRadius: 1,
+            bgcolor: '#DCDFE4',
+            color: 'text.primary',
+            px: 2,
+            gap: 1,
+          }}
+        >
+          <PersonAddAltIcon fontSize="small" />
+          Share
+        </IconButton>
+        <Menus />
+      </Box>
     </Box>
   )
 }
